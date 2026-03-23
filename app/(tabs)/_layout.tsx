ec3f4073b847e9b980e7dc6e4e73be73a5f2abec
matthewzim <53016744+matthewzim@users.noreply.github.com>
2026-03-23 13:51:22 -0700

    Fix text input keyboard handling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,7 +44,6 @@ export default function TabsLayout() {
         headerTitleStyle: {
           fontFamily: getFontFamily('semibold'),
         },
-        keyboardHandlingEnabled: false,
       }}
     >
       <Tabs.Screen
